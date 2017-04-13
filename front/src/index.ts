d9e55033b39fb0874f3008fd659387cc62316503
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-14 00:05:26 +0900

    update files for debug running

--- front/src/index.ts
+++
@@ -1,8 +0,0 @@
-import 'core-js';
-import 'zone.js/dist/zone';
-import 'rxjs/Rx';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
