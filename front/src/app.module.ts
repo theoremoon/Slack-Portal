909bdc3e53b5d36297ab502cd397671d04c8dccb
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-04-13 22:56:35 +0900

    add login component

--- front/src/app.module.ts
+++ front/src/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from "@angular/router";
 import { WebSocketService } from './websocket.service';
 import { UpdateNotifyService } from './update-notify.service';
 import { TeamsComponent } from './teams.component';
 import { AudioService } from './audio.service';
 import { FlashService } from './flash.service';
 
+const appRoutes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', component: TeamsComponent }
+];
+
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [ 
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
     declarations: [ 
         AppComponent,
         TeamsComponent
